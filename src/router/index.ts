78e6f725c9750c69cd299e36532a2f071f12b928
cql <cql.ql@qq.com>
2022-03-24 18:05:49 +0800

    feat: 部署静态页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,18 +23,18 @@ const routes: Array<AppRouteRecordRaw> = [
       title: 'home',
     },
   },
-  {
-    path: '/about',
-    name: 'about',
-    meta: {
-      title: 'about',
-    },
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   meta: {
+  //     title: 'about',
+  //   },
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  // },
   LoginRoute,
 ]
 
