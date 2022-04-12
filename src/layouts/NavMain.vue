70e1d7c50ff69b194b394f3eac02e0bd0e8e2ac0
cql <cql.ql@qq.com>
2022-04-12 16:33:19 +0800

    layout

--- src/layouts/NavMain.vue
+++ src/layouts/NavMain.vue
@@ -1,19 +1,36 @@
 <script lang="ts" setup>
 import Icon from '@/components/Icon/src/Icon.vue'
+import { ref } from 'vue'
+
+const selectedIndex = ref(0)
+
+const list = [
+  {
+    name: 'Trade',
+    icon: 'ant-design:rise-outlined',
+  },
+  {
+    name: 'Finance',
+    icon: 'icon-park-outline:funds',
+  },
+  {
+    name: 'Finance',
+    icon: 'fa6-solid:handshake',
+  },
+]
 </script>
 <template>
   <nav class="nav-main">
-    <a class="item trade active" href="/">
-      <i> <Icon icon="ant-design:rise-outlined"></Icon> </i>
-      <span class="name">Trade</span>
-    </a>
-    <a class="item finance" href="/">
-      <i> <Icon icon="icon-park-outline:funds"></Icon> </i>
-      <span class="name">Finance</span>
-    </a>
-    <a class="item finance" href="/">
-      <i> <Icon icon="fa6-solid:handshake"></Icon> </i>
-      <span class="name">Finance</span>
+    <a
+      v-for="(item, index) of list"
+      :key="index"
+      class="item"
+      :class="{ active: selectedIndex === index }"
+      href="javascript:;"
+      @click="selectedIndex = index"
+    >
+      <i> <Icon :icon="item.icon"></Icon> </i>
+      <span class="name">{{ item.name }}</span>
     </a>
   </nav>
 </template>
@@ -23,6 +40,7 @@ import Icon from '@/components/Icon/src/Icon.vue'
   & > .item {
     position: relative;
     display: block;
+    display: flex;
     width: 110px;
     height: 110px;
     color: #7d8da1;
@@ -38,21 +56,29 @@ import Icon from '@/components/Icon/src/Icon.vue'
       height: 100%;
       content: '';
       opacity: 0;
-      background-color: #3099f5;
+      background-color: @primary-color;
       transition: opacity 0.3s, left 0.3s, right 0.3s;
     }
 
-    &.active::before {
-      opacity: 1;
-      left: 0;
+    &.active {
+      color: @primary-color;
+
+      &::before {
+        opacity: 1;
+        left: 0;
+      }
     }
 
     i {
-      display: flex;
-      align-items: flex-end;
-      justify-content: center;
-      height: 60px;
+      // display: flex;
+      // align-items: flex-end;
+      // justify-content: center;
+      // height: 60px;
       font-size: 38px;
+    }
+
+    .app-iconify {
+      display: block;
     }
 
     .name {
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
+const path = require('path')
+const getLessVariables = require('./build/getLessVariables')
 module.exports = {
   publicPath: '',
   css: {
     extract: false,
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          globalVars: getLessVariables(
+            path.relative(__dirname, 'src/style/color.less'),
+          ),
+        },
+      },
+    },
   },
 }
