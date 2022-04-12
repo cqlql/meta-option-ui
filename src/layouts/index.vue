e61ae1dc3ae2a450896cdbc908b73f79de2efb6c
陈桥黎 <cql.ql@qq.com>
2022-04-12 11:07:24 +0800

    layout

---
+++ src/layouts/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import PageLayout from './page/PageLayout.vue'
+import { Layout } from 'ant-design-vue'
+</script>
+<template>
+  <!-- <div class="layout"> div.layout-header </div> -->
+
+  <div>
+    <Layout>
+      <Layout.Header>header</Layout.Header>
+      <Layout>
+        <Layout.Sider>left sidebar</Layout.Sider>
+        <Layout.Content> <PageLayout></PageLayout></Layout.Content>
+      </Layout>
+      <Layout.Footer>footer</Layout.Footer>
+    </Layout>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,41 +2,48 @@ import type { RouteRecordRaw } from 'vue-router'
 import type { AppRouteRecordRaw } from './types'
 import { createRouter, createWebHashHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
-
-export const LoginRoute: AppRouteRecordRaw = {
-  path: '/login',
-  name: 'Login',
-  component: () =>
-    import(/* webpackChunkName: "index" */ '@/views/Login/LoginView.vue'),
-  meta: {
-    title: '登录',
-  },
-}
+import LAYOUT from '@/layouts/index.vue'
+import FinaceView from '@/views/Finance/FinaceView.vue'
 
 const routes: Array<AppRouteRecordRaw> = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   redirect: '/login',
+  //   // component: HomeView,
+  //   meta: {
+  //     title: 'home',
+  //   },
+  // },
   {
     path: '/',
     name: 'home',
-    redirect: '/login',
+    component: LAYOUT,
+    redirect: '/finance',
     // component: HomeView,
     meta: {
-      title: 'home',
+      // title: 'home',
+    },
+    children: [
+      {
+        path: 'finance',
+        name: 'finance',
+        component: FinaceView,
+        meta: {
+          title: 'finance',
+        },
+      },
+    ],
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () =>
+      import(/* webpackChunkName: "index" */ '@/views/Login/LoginView.vue'),
+    meta: {
+      title: '登录',
     },
   },
-
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   meta: {
-  //     title: 'about',
-  //   },
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () =>
-  //     import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  // },
-  LoginRoute,
 ]
 
 const router = createRouter({
