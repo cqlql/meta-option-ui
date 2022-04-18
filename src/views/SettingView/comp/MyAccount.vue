616780c283c781425d0b04a5230c028dab208de3
cql <cql.ql@qq.com>
2022-04-18 20:53:13 +0800

    setting - My Account

--- src/views/SettingView/comp/MyAccount.vue
+++ src/views/SettingView/comp/MyAccount.vue
@@ -2,6 +2,8 @@
 import TitleItem from '@/views/Finance/comp/TitleItem.vue'
 import AccountBalance from './AccountBalance.vue'
 import BalanceList from './BalanceList.vue'
+import CheckboxPlus from '@/components/CheckboxPlus/CheckboxPlus.vue'
+import Icon from '@/components/Icon/src/Icon.vue'
 </script>
 <template>
   <div>
@@ -11,8 +13,54 @@ import BalanceList from './BalanceList.vue'
           <AccountBalance />
         </TitleItem>
       </div>
-      <div class="flex-[1.2]">
-        <TitleItem title="Verification"> </TitleItem>
+      <div class="flex-[1.2] ml-20">
+        <TitleItem title="Verification">
+          <div class="Verification">
+            <div class="text"> By completing verification, you </div>
+            <ul class="text-list">
+              <li> Pay with UPI </li>
+              <li> Trade on Market </li>
+            </ul>
+
+            <CheckboxPlus
+              cancellable
+              :list="[{ name: '1' }, { name: '2' }]"
+              class="Checkbox"
+            >
+              <template #1>
+                <div class="Checkbox_item">
+                  <div class="img">
+                    <Icon icon="mdi:card-account-details-outline"></Icon>
+                  </div>
+                  <div class="text">
+                    <div class="label">Photo ID</div>
+                    <div class="value">
+                      Passport, National ID, or Driver’s license.
+                    </div>
+                  </div>
+                </div>
+              </template>
+              <template #2>
+                <div class="Checkbox_item">
+                  <div class="img">
+                    <Icon icon="ri:user-3-fill"></Icon>
+                  </div>
+                  <div class="text">
+                    <div class="label">Selfie</div>
+                    <div class="value">
+                      The photo ID and your face must be cleary visible without
+                      cropping
+                    </div>
+                  </div>
+                </div>
+              </template>
+            </CheckboxPlus>
+
+            <div class="text-right mt-10">
+              <a-button type="primary" class="big !px-20">Upload</a-button>
+            </div>
+          </div>
+        </TitleItem>
       </div>
     </div>
   </div>
@@ -20,5 +68,62 @@ import BalanceList from './BalanceList.vue'
 
 <style lang="less" scoped>
 .MyAccount {
+  .Verification {
+    margin-top: -16px;
+
+    & > .text {
+    }
+
+    & > .text-list {
+      margin-bottom: -6px;
+
+      li::before {
+        content: '';
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        background-color: #389bf2;
+        border-radius: 10px;
+        box-shadow: 0 0 10px 0 #389bf2;
+        margin-right: 20px;
+      }
+    }
+
+    .Checkbox {
+      // display: grid;
+      // grid-template-columns: 1fr 1fr;
+      // grid-row-gap: 20px;
+      // grid-column-gap: 20px;
+      display: block;
+
+      & > ::v-deep(.item) {
+        padding: 20px;
+        margin-top: 32px;
+      }
+
+      // & > ::v-deep(.item + .item) {
+      //   margin-top: 32px;
+      // }
+    }
+
+    .Checkbox_item {
+      display: flex;
+      color: #fff;
+      font-size: 18px;
+
+      .img {
+        font-size: 50px;
+      }
+
+      .text {
+        margin-left: 30px;
+      }
+
+      .label {
+        line-height: 46px;
+        font-size: 24px;
+      }
+    }
+  }
 }
 </style>
