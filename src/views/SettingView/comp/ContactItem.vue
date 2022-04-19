a3134cf2b1e3fbfe4f6970679b4bf7c6b5bb6cbd
陈桥黎 <cql.ql@qq.com>
2022-04-19 13:50:37 +0800

    Setting Contact

---
+++ src/views/SettingView/comp/ContactItem.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="ContactItem">
+    <div class="left">
+      <img src="" alt="" />
+    </div>
+    <div class="right">
+      <div class="title">Contact me on telegram</div>
+      <div class="number"> (+00) 123 456 789 </div>
+      <div class="btn text-right">
+        <a-button>Go</a-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.ContactItem {
+  display: flex;
+
+  .left {
+    width: 310px;
+    height: 220px;
+  }
+
+  .right {
+    flex: 1;
+  }
+}
+</style>
--- src/views/SettingView/comp/ContactView.vue
+++ src/views/SettingView/comp/ContactView.vue
@@ -1,6 +1,40 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ContactItem from './ContactItem.vue'
+</script>
 <template>
-  <div class="ContactView"> ContactView </div>
+  <div class="ContactView">
+    <div class="left">
+      <ContactItem></ContactItem>
+      <ContactItem></ContactItem>
+      <ContactItem></ContactItem>
+    </div>
+    <div class="right">
+      <div class="phone">
+        <div class="img">
+          <img src="" alt="" />
+        </div>
+        <div class="title"> Phone Number </div>
+        <div class="number">(+00) 123 456 789 / (+00) 234 999 888</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="less" scoped>
+.ContactView {
+  display: flex;
+
+  .left {
+  }
+
+  .right {
+  }
+
+  .phone {
+    background-color: #22293f;
+    box-shadow: 0 10px 58px 0 rgba(0, 0, 0, 0.24);
+    border-radius: 6px;
+    border: solid 2px #424b60;
+  }
+}
+</style>
