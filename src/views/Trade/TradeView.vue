93416e4844663346ce8d0e836b859ebbcb4731ba
陈桥黎 <cql.ql@qq.com>
2022-04-26 15:57:42 +0800

    trade

--- src/views/Trade/TradeView.vue
+++ src/views/Trade/TradeView.vue
@@ -2,6 +2,8 @@
 import { ref } from 'vue'
 import { Select as ASelect } from 'ant-design-vue'
 import IconFont from '@/components/IconFont/IconFont.vue'
+import InputNumber from './comp/InputNumber.vue'
+import Icon from '@/components/Icon/src/Icon.vue'
 
 const assetValue = ref('EUR/USD')
 const options = [
@@ -31,7 +33,68 @@ const options = [
       </div>
     </div>
 
-    <div class="deal-controller"></div>
+    <div class="deal-controller">
+      <div class="left">
+        <div class="row-top">
+          <div class="item">
+            <div class="title">Amount</div>
+            <div class="ipt">
+              <InputNumber> </InputNumber>
+            </div>
+          </div>
+          <div class="item">
+            <div class="title">Strike rate</div>
+            <div class="ipt">
+              <InputNumber> </InputNumber>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="btn">
+            <div><button class="active">60s</button></div>
+            <div class="text">Profit 80%</div>
+          </div>
+          <div class="btn">
+            <div><button>60s</button></div>
+            <div class="text">Profit 80%</div>
+          </div>
+          <div class="btn">
+            <div><button>60s</button></div>
+            <div class="text">Profit 80%</div>
+          </div>
+          <div class="btn">
+            <div><button>60s</button></div>
+            <div class="text">Profit 80%</div>
+          </div>
+          <div class="btn">
+            <div><button>60s</button></div>
+            <div class="text">Profit 80%</div>
+          </div>
+        </div>
+      </div>
+      <div class="right">
+        <div class="item">
+          <div class="title">$91.50</div>
+          <div class="btn">
+            <button
+              ><Icon icon="ic:baseline-keyboard-arrow-down"></Icon>
+              <span>DOWN</span></button
+            >
+            <div class="info">95%</div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="title">$91.50</div>
+          <div class="btn">
+            <button class="up"
+              ><Icon icon="ic:baseline-keyboard-arrow-up"></Icon>
+              <span>UP</span></button
+            >
+            <div class="info">95%</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -85,14 +148,119 @@ const options = [
     left: 50%;
     bottom: 30px;
     z-index: 10;
-    width: 525px;
-    height: 80px;
-    padding: 0 10px;
+    display: flex;
+    width: 740px;
+    // height: 80px;
+    padding: 6px 15px;
     margin-left: -262.5px;
     border-radius: 5px;
     background-color: rgba(28, 35, 51, 0.6);
     box-sizing: border-box;
     transition: width 0.2s, margin 0.2s;
+    gap: 15px;
+
+    & > .left {
+      flex: 2;
+
+      .btn {
+        // display: inline-block;
+        // padding: 6px 10px;
+      }
+
+      .row {
+        display: flex;
+        padding-top: 6px;
+        gap: 10px;
+      }
+
+      .row-top {
+        display: flex;
+        gap: 15px;
+      }
+
+      .item {
+        flex: 1;
+      }
+
+      button {
+        border-radius: 4px;
+        border: solid 1px #424b60;
+        height: 27px;
+        padding: 0 15px;
+
+        &:hover {
+          border-color: #389bf2;
+        }
+
+        &.active {
+          border-color: #389bf2;
+          background-color: #389bf2;
+        }
+      }
+
+      .text {
+        font-size: 12px;
+      }
+    }
+
+    & > .right {
+      flex: 1;
+      display: flex;
+      gap: 12px;
+
+      .item {
+        flex: 1;
+
+        // &:first-child {
+        //   padding-right: 6px;
+        // }
+
+        // &:last-child {
+        //   padding-left: 6px;
+        // }
+      }
+
+      .btn {
+      }
+
+      .info {
+      }
+
+      button {
+        width: 126px;
+        height: 78px;
+        color: rgba(0, 0, 0, 0.3);
+        font-size: 18px;
+        background-image: linear-gradient(-180deg, #d4225d, #d42260);
+        font-weight: bold;
+        border-radius: 4px;
+        transition: transform 0.2s, opacity 0.2s;
+
+        &:hover {
+          opacity: 0.8;
+        }
+
+        &:active {
+          transform: translateY(1px);
+        }
+
+        span {
+          vertical-align: middle;
+        }
+
+        .app-iconify {
+          font-size: 42px;
+        }
+      }
+
+      .up {
+        background-image: linear-gradient(-180deg, #8cc44f, #4ed462);
+      }
+    }
+
+    .title {
+      color: #424b60;
+    }
   }
 }
 </style>
---
+++ src/views/Trade/comp/InputNumber.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+import Icon from '@/components/Icon/src/Icon.vue'
+</script>
+<template>
+  <div class="InputNumber">
+    <div class="action left">
+      <Icon icon="ic:baseline-minus"></Icon>
+    </div>
+    <div class="middle">
+      <input type="text" />
+    </div>
+    <div class="action right">
+      <Icon icon="ic:baseline-add"></Icon>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.InputNumber {
+  display: flex;
+  border: 1px solid #424b60;
+  border-radius: 4px;
+
+  .action {
+    width: 46px;
+    height: 46px;
+    line-height: 46px;
+    font-size: 22px;
+    text-align: center;
+  }
+
+  .left {
+  }
+
+  .right {
+  }
+
+  .middle {
+    flex: 1;
+    border: 1px solid #414a60;
+    border-top: none;
+    border-bottom: none;
+
+    input {
+      background: transparent;
+      border-radius: 0;
+    }
+  }
+}
+</style>
